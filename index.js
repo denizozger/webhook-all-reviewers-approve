9670febc1a6c7b7b23b3f67fb8bb2e5df673b553
Deniz Ozger <deniz@klarismo.com>
2016-11-11 13:51:44 +0000

    Cast port to number

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ 
     host: 'localhost', 
-    port: process.env.PORT || 8000 
+    port: Number(process.env.PORT) || 8000 
 });
 
 // Add the route
